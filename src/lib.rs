4715bf4328ffa410b55a0eb0aa251cf048a6b38b
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2026-01-09 12:21:59 -0900

    add styles & reactivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
+use floem::IntoView;
 use floem::prelude::*;
-use floem::{views::button, IntoView};
+use floem::views::{Decorators, button, dyn_view};
 
 fn app_view() -> impl IntoView {
     let mut counter = RwSignal::new(0);
-    ("Value: ", button("Increment"), button("Decrement"))
+    (
+        dyn_view(move || format!("Value: {}", counter)),
+        (
+            button("Increment").action(move || counter += 1),
+            button("Decrement").action(move || counter -= 1),
+        )
+            .style(|s| s.flex_row().gap(6)),
+    )
+        .style(|s| s.flex_col().gap(6).items_center())
 }
 
 pub fn launch_app() {
